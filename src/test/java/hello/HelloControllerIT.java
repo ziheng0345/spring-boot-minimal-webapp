ea0b4e5703307ab61fca24ab53bf553d4b30dcd4
Ziheng Zhao <ziheng@umail.ucsb.edu>
2019-10-05 16:43:32 -0700

    fixed bugs and passed the test

--- src/test/java/hello/HelloControllerIT.java
+++ src/test/java/hello/HelloControllerIT.java
@@ -36,6 +36,8 @@ public class HelloControllerIT {
     public void getHello() throws Exception {
         ResponseEntity<String> response = template.getForEntity(base.toString(),
                 String.class);
-        assertThat(response.getBody(), equalTo("Greetings from Spring Boot!"));
+        assertThat(response.getBody(), equalTo("<h1>Hello World!</h1>\n" +
+    "<p>This web app is powered by \n" +
+    "<a href='https://github.com/ziheng0345/spring-boot-minimal-webapp'>this github repo</a></p>\n"));
     }
 }
--- src/test/java/hello/HelloControllerTest.java
+++ src/test/java/hello/HelloControllerTest.java
@@ -26,6 +26,8 @@ public class HelloControllerTest {
     public void getHello() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+                .andExpect(content().string(equalTo("<h1>Hello World!</h1>\n" +
+    "<p>This web app is powered by \n" +
+    "<a href='https://github.com/ziheng0345/spring-boot-minimal-webapp'>this github repo</a></p>\n")));
     }
 }
