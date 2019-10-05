9acde0f57a611b55974d680e34a736c80f1b3bad
Ziheng Zhao <ziheng@umail.ucsb.edu>
2019-10-05 16:21:50 -0700

    ?

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,13 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+      /*  return "Greetings from Spring Boot!";
+    */
+
+   	String html= "<h1>Hello World!</h1>\n"+
+	"<p>This web app is powered by \n"+
+	"<a href='https://github.com/ziheng0345/spring-boot-minimal-webapp'>this github repo</a></p>\n";
+	return html;
     }
     
 }
